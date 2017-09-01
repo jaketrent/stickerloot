c96c54bfa805a24c4465199d16cdb32cd7bceb39
jaketrent <trent.jake@gmail.com>
2017-09-01 07:25:39 -0600

    feat: create shared component location

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import React from 'react'
 import ReactDOM from 'react-dom/server'
 
+import Component from '../../shared/dist/component'
+
 const app = express()
 const port = process.env.PORT || 3001
 
 app.get('*', (req, res) => {
-  res.send(ReactDOM.renderToString(<h1>Hola, Reactish</h1>))
+  res.send(ReactDOM.renderToString(<Component />))
 })
 
 app.listen(port, _ => console.log(`Listening on ${port}...`))
