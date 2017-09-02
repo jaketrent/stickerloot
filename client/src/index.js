84681061a9816dd915b89a25c561a1966b0f588e
jaketrent <trent.jake@gmail.com>
2017-09-02 16:17:08 -0600

    feat: shared module alias; watchify really rebuilds

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { rehydrate } from 'glamor'
 
-import Component from '../../shared/dist/component'
+import Component from '@shared/component'
 
-console.log('client!', window._glam)
+console.log('wow!6')
 rehydrate(window._glam)
 ReactDOM.render(<Component />, document.getElementById('app'))
