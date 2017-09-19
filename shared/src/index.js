ac04f374c23905a322998f9983e0a38bfc637cea
jaketrent <trent.jake@gmail.com>
2017-09-19 17:59:31 -0600

    feat: real instructions on the welcome page

--- shared/src/index.js
+++ shared/src/index.js
@@ -1,83 +1,35 @@
 import glamorous from 'glamorous'
 import React from 'react'
 
-import Actions from './ui/actions'
-import GameOver from './ui/game-over'
-import Pinata from './ui/pinata'
-import Stats from './ui/stats'
-import * as stickers from './util/stickers'
-import Stickers from './ui/stickers'
 import TopNav from './ui/topnav'
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hitsRemaining: 3,
-      crackedStickers: [],
-      // TODO: maybe consolidate on crackedStickers state?
-      isCracked: false,
-      swingsCount: 0,
-      swingsRemaining: 8,
-      stickers: stickers.initStartingSet()
-    }
-    this.handleReset = this.handleReset.bind(this)
-    this.handleSwing = this.handleSwing.bind(this)
-    this.handleTrade = this.handleTrade.bind(this)
-  }
-  handleReset() {
-    this.setState({
-      hitsRemaining: 3,
-      crackedStickers: [],
-      isCracked: false
-    })
-  }
-  handleSwing(isHit) {
-    const hitsRemaining = this.state.hitsRemaining - (isHit ? 1 : 0)
-    const crackedStickers = stickers.random()
-    this.setState({
-      hitsRemaining,
-      swingsCount: this.state.swingsCount + 1,
-      swingsRemaining: this.state.swingsRemaining - 1,
-      ...(hitsRemaining === 0
-        ? {
-            crackedStickers,
-            isCracked: true,
-            stickers: crackedStickers.reduce((acc, sticker) => {
-              acc[sticker.id] = {
-                ...this.state.stickers[sticker.id],
-                count: this.state.stickers[sticker.id].count + 1
-              }
-              return acc
-            }, this.state.stickers)
-          }
-        : {})
-    })
-  }
-  handleTrade() {
-    this.setState({
-      swingsRemaining: this.state.swingsRemaining + 8,
-      stickers: Object.keys(this.state.stickers).reduce((acc, id) => {
-        acc[id] = {
-          ...this.state.stickers[id],
-          count: this.state.stickers[id].count - 1
-        }
-        return acc
-      }, this.state.stickers)
-    })
-  }
-  render() {
-    return (
-      <div>
-        <TopNav gameIds={this.props.serverState.gameIds} />
-        <h1>Welcome</h1>
-        <p>
-          Here's how you play
-        </p>
-        <p>
-          Start a new game
-        </p>
-      </div>
-    )
-  }
-}
+const Content = glamorous.div({
+  padding: '20px'
+})
+
+export default props =>
+  <div>
+    <TopNav gameIds={props.serverState.gameIds} />
+    <Content>
+      <h1>Welcome!</h1>
+      <h2>To start</h2>
+      <p>
+        Whack the pinata with exceeding force. 3 hits will crack open the
+        pinata. When cracked open, the pinata will shower you with stickers.
+      </p>
+      <p>
+        Trade a set of stickers in for more whacks at the pinata.
+      </p>
+      <p>
+        When you are out of swings and out of sets of stickers, the party will
+        come to an end.
+      </p>
+      <p>
+        Select 'New' in the menu above. Round uno, fight!
+      </p>
+      <h2>To stop</h2>
+      <p>
+        Modify this code until it breaks.
+      </p>
+    </Content>
+  </div>
