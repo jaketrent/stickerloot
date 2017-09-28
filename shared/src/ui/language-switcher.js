491d5c31e35d2ecb5c08c530bf9e7578e67e4de4
jaketrent <trent.jake@gmail.com>
2017-09-28 14:02:20 -0500

    refactor: restyle language switcher

--- shared/src/ui/language-switcher.js
+++ shared/src/ui/language-switcher.js
@@ -6,50 +6,50 @@ const bgColor = '#caaa65'
 
 const LanguageDiv = glamorous.div({
   display: 'inline-flex',
-  alignItems: 'center',
-  backgroundColor: '#caaa65',
-  color: '#fff',
-  padding: '0.5em 1em',
-  borderRadius: '4px',
-  fontSize: '1.125em'
+  alignItems: 'center'
 })
 
 const LangOpt = glamorous.button(
   {
-    padding: '2px 4px',
+    padding: '4px 8px',
     fontSize: '1em',
     fontWeight: 'bold',
     textDecoration: 'none',
     transition: 'all 300ms',
-    textShadow: '1px 1px 0 rgba(0, 0, 0, 0.5)',
     border: 'none',
-    background: 'none',
     cursor: 'pointer',
-    ':hover': {
-      color: '#49c63b'
+    textShadow: '1px 1px 0 rgba(0, 0, 0, 0.5)',
+    color: '#fff',
+    ':hover': { backgroundColor: '#E41D47' },
+    ':first-child': {
+      borderRadius: '4px 0 0 4px'
+    },
+    ':last-child': {
+      borderRadius: '0 4px 4px 0'
     }
   },
   ({ isActive }) =>
     isActive
       ? {
-          color: '#49c63b'
+          backgroundColor: '#49c63b'
         }
-      : { color: '#fff' }
+      : {
+          backgroundColor: '#caaa65'
+        }
 )
 
 const langs = ['en', 'es']
 
 const Language = (props, context) => (
   <LanguageDiv>
-    {langs.map((lang, i) => [
+    {langs.map(lang => [
       <LangOpt
         key={lang}
         isActive={lang === context.currentLang}
         onClick={_ => props.onSelect(lang)}
       >
         {lang}
-      </LangOpt>,
-      i !== langs.length - 1 ? '/' : null
+      </LangOpt>
     ])}
   </LanguageDiv>
 )
